9dd533ad5201758d27a7ec68f576e7739f63ab8c
Ayad Farhat <ayad.farhat2001@gmail.com>
2024-11-16 19:24:56 +0700

    fixed deployement script

--- script/Deposit.s.sol
+++ script/Deposit.s.sol
@@ -5,23 +5,20 @@ import "forge-std/Script.sol";
 import "../src/Deposit.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MockERC20 is ERC20 {
-    constructor() ERC20("Mock Token", "MCK") {
-        _mint(msg.sender, 1000000 * 10 ** 18);
-    }
-}
-
+address constant USDC_BASE_SEPOLIA_TOKEN_ADDRESS = 0x2e880FbE609947Cd45D0B4dA22caa1B6777ba9A4;
 contract DeployNanoChai is Script {
     function run() external {
 
-        // Start broadcasting
-        vm.startBroadcast();
-
-        // Deploy mock ERC20 token
-        MockERC20 mockToken = new MockERC20();
+        // Start broadcasting'
+        
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        string memory rpcUrl = vm.envString("BASE_SEPOLIA_RPC_URL");
+        vm.createSelectFork(rpcUrl);
+        vm.startBroadcast(deployerPrivateKey);
 
         // Deploy the NanoChai contract with mock token
-        NanoChai nanoChai = new NanoChai(mockToken);
+        NanoChai nanoChai = new NanoChai(USDC_BASE_SEPOLIA_TOKEN_ADDRESS);
+        console.log("NanoChai contract deployed at address: %s", address(nanoChai));
 
         // Stop broadcasting
         vm.stopBroadcast();
